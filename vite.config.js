9fda963d2dd37d676c49cd77c892bbdc23d2a90d
reynaVite <20210719@uthh.edu.mx>
2024-09-27 00:04:41 -0600

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,82 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ['favicon.ico', "apple-touch-icon.png", "masked-icon.png"],
+  manifest: {
+    name: "EduZona",
+    short_name: "EduZona",
+    description: "EduZona",
+    icons: [
+      {
+        src: "./icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "./icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: 'favicon'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: "./icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon"
+      },
+      {
+        src: "./icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'EduZona',
-        short_name: 'EduZona',
-        description: 'Mi increíble aplicación React con soporte PWA y Vite',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'img/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'img/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
+      ...manifestForPlugin,
       workbox: {
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Ajustar el tamaño máximo a 5 MB
+        maximumFileSizeToCacheInBytes: 3145728, // Ajusta según tus necesidades
       },
-    }),
+    })
   ],
   build: {
+    chunkSizeWarningLimit: 1000, // Ajusta el límite de advertencia de tamaño de chunk
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; // Agrupa todos los módulos de node_modules en un chunk llamado "vendor"
+            return 'vendor'; // Agrupa todas las dependencias en un chunk
           }
-          // Puedes agregar más agrupaciones aquí si es necesario
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Ajusta el límite de advertencia de tamaño de chunk
-  },
+          // Agrega más condiciones si es necesario
+        }
+      }
+    }
+  }
 });
