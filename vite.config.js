1f33f02e3fed3b4de7e24bb9ab3a1381a8af8b2a
reynaVite <20210719@uthh.edu.mx>
2024-09-26 22:23:36 -0600

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,48 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import WindiCSS from 'vite-plugin-windicss';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'EduZona',
+        short_name: 'EduZona',
+        description: 'Mi increíble aplicación React con soporte PWA y Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'img/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Ajustar el tamaño máximo a 5 MB
+      },
+    }),
+  ],
   build: {
-    outDir: 'builds', // Cambia 'builds' al nombre de la carpeta que prefieras
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Agrupa todos los módulos de node_modules en un chunk llamado "vendor"
+          }
+          // Puedes agregar más agrupaciones aquí si es necesario
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Ajusta el límite de advertencia de tamaño de chunk
   },
 });
