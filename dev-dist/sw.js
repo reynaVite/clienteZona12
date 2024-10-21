57b076a5ac56a9326dcef5f42ef1a4a02af0d7e0
reynaVite <20210719@uthh.edu.mx>
2024-10-20 20:04:35 -0600

    prueba 1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-863f9d56'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g23v8kns9ug"
+    "revision": "0.8adn9l58bqo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,32 +1,28 @@
-importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js")
-importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js");
 
-
- 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgREgP0o0aZa1VRThhKKqXHgzl6xL8Zog",
-  authDomain: "eduzona-ba7dd.firebaseapp.com",
-  projectId: "eduzona-ba7dd",
-  storageBucket: "eduzona-ba7dd.appspot.com",
-  messagingSenderId: "761792972433",
-  appId: "1:761792972433:web:5defaa01f3f73bcc916130",
-  measurementId: "G-Y7D8QQ9JR0"
-}; 
+    apiKey: "AIzaSyAgREgP0o0aZa1VRThhKKqXHgzl6xL8Zog",
+    authDomain: "eduzona-ba7dd.firebaseapp.com",
+    projectId: "eduzona-ba7dd",
+    storageBucket: "eduzona-ba7dd.appspot.com",
+    messagingSenderId: "761792972433",
+    appId: "1:761792972433:web:5defaa01f3f73bcc916130",
+    measurementId: "G-Y7D8QQ9JR0"
+  };
+  
+const app = firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging(app)
 
-
-const app = firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging(app);
-
-
-messaging.onBackgroundMessage(payload => {
-    console.log("Recibiste mensaje mientras estabas ausente"); 
-    const notificationTitle= payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        
-    }
-    return self.registration.showNotification(
-        notificationTitle, 
-        notificationOptions
-    )
+messaging.onBackgroundMessage((payload) => {
+  console.log('Mensaje recibido en segundo plano: ', payload)
+  console.log(payload)
+  
+  const notificationTitle = payload.notification.title
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "https://labarbada.store/img/logo.png"
+  }
+  
+  return self.registration.showNotification(notificationTitle, notificationOptions)
 })
