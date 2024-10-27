e10c52357eb8c8d9e0251ba77c4c49736f73736f
Jesus Antonio Ramirez <tonchirmz@gmail.com>
2024-10-27 02:29:57 -0600

    Subiendo archivo YML y otras cosas mas

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+};
